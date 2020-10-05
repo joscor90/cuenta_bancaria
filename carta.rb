bbc1f365f363b180c78fc42ab20a7b8ab75513f4
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-10-05 10:42:33 -0300

    corrected various errors in both files

--- carta.rb
+++ carta.rb
@@ -1,11 +1,31 @@
 class Carta
     attr_accessor :numero, :pinta
-    def initialize
-        pintas = ['C', 'D', 'T', 'E']
-        @numero = Random.rand(1...13)
-        @pinta = pintas.sample
+    def initialize(numero=Random.rand(1...13), pinta=['C', 'D', 'T', 'E'].sample)
+        if numero > 13 || numero < 1
+            raise RangeError, "valor fuera de rango"
+        end
+        @numero = numero
+        if ['C', 'D', 'T', 'E'].include?(pinta) == false
+            raise ArgumentError, "valor incorrecto"
+        end
+        @pinta = pinta
     end
 end
+
+#Instanciando carta pasando parámetros
+c1 = Carta.new(13, 'D')
+pp c1
+
+#Instanciando carta con valor por defecto.
+c2 = Carta.new
+pp c2
+
+#Probando manejo de excepciones
+# c3 = Carta.new(14, 'C')
+# c4 = Carta.new(0, 'D')
+# c5 = Carta.new(1, 'X')
+
+#Probando la clase creando 5 objetos y empujándolos a un arreglo.
 
 cartas = []
 
@@ -13,4 +33,4 @@ cartas = []
     cartas.push(Carta.new)
 end
 
-pp cartas
+print "#{cartas}\n"
--- cuenta_bancaria.rb
+++ cuenta_bancaria.rb
@@ -1,29 +1,25 @@
 class CuentaBancaria
     attr_accessor :nombre_de_usuario
-    attr_reader :numero_de_cuenta, :vip
-    def initialize(nombre_de_usuario, numero_de_cuenta, vip = 0)
+    attr_reader :numero_cuenta, :vip
+    def initialize(nombre_de_usuario, numero_cuenta, vip = 0)
         @nombre_de_usuario = nombre_de_usuario
-        @numero_de_cuenta = numero_de_cuenta
-        raise RangeError if @numero_de_cuenta.digits.count != 8
+        @numero_cuenta = numero_cuenta
+        raise RangeError if numero_cuenta.digits.count != 8
         @vip = vip
+    end
+
+    def numero_de_cuenta
+        "#{self.vip}-#{self.numero_cuenta}"
     end
 end
 
-def numero_de_cuenta(cuenta)
-    if cuenta.vip == 1
-        cuenta.numero_de_cuenta.to_s
-        vip_member = "1-#{cuenta.numero_de_cuenta}"
-    elsif cuenta.vip == 0
-        cuenta.numero_de_cuenta.to_s
-        vip_member = "0-#{cuenta.numero_de_cuenta}"
-    end
-    return vip_member
-end
+
 
 #test variable
 c1 = CuentaBancaria.new('jose', 12345678, 1)
+pp c1
 
 #testing numero_de_cuenta
-a = numero_de_cuenta(c1)
+pp c1.numero_de_cuenta
 
 
